8eaa5f2561300075f059ca6caa7642ea70b26322
iamcheeseman <xanderswan32@gmail.com>
2022-09-22 17:45:30 -0400

    Renamed `FrameTimer` to `AwaitTimer`

--- tools/timer/await_timer.gd
+++ tools/timer/await_timer.gd
@@ -1,5 +1,5 @@
 extends Node
-class_name FrameTimer
+class_name AwaitTimer
 
 class _PhysicsTimer extends Node:
 	var _time_left := 1
@@ -14,7 +14,7 @@ class _PhysicsTimer extends Node:
 		if _time_left <= 0:
 			emit_signal("timeout")
 
-static func physics_timer(node: Node, time:=1) -> _PhysicsTimer:
+static func timer(node: Node, time:=1) -> _PhysicsTimer:
 	var new_timer = _PhysicsTimer.new(time)
 	node.add_child(new_timer)
 	return new_timer
